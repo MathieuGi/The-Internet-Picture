3f73779e1e2781de22f1e9115ed974184614a01b
MathieuGi <math.girardon@hotmail.fr>
2017-09-20 18:41:20 +0200

    Get current and notime button in "/" route

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,42 @@
 var express = require('express');
 var router = express.Router();
 var bidService = require('../services/bids');
-var buttonBuys = require('../services/buttonBuys');
+var buttonBuyService = require('../services/buttonBuys');
 var winston = require('winston');
 var FILE_NAME = "routes/index.js";
 
-/* GET home page. */ 
+/* GET home page. */
 router.get('/', function(req, response, next) {
 
-    var queries = [bidService.getBest(), bidService.getAll(),buttonBuys.getCurrentButton(),buttonBuys.noTimeButton()];
+    var queries = [
+        bidService.getBest(),
+        bidService.getAll(),
+        buttonBuyService.getCurrentButton(),
+    ];
 
-    var mostRecentPromise = queries.reduce((previousPromise, query) => {
-        return previousPromise.then(resultsArray => {
-            console.log(previousPromise+"--------------------------");
-            return query.then(res => {
-                resultsArray.push(res);
-                return resultsArray;
-            }).catch(err => {
-                winston.info(FILE_NAME + ' - Nothing retrieve from database')
-                resultsArray.push(null);
-                return resultsArray;
-            });
+    var mostRecentPromise = function(array) {
+        return array.reduce((previousPromise, query) => {
+            return previousPromise.then(resultsArray => {
+                return query.then(res => {
+                    resultsArray.push(res);
+                    return resultsArray;
+                }).catch(err => {
+                    winston.info(FILE_NAME + ' - Nothing retrieve from database')
+                    resultsArray.push(null);
+                    return resultsArray;
+                });
+            })
+        }, Promise.resolve([]));
+    }
+
+    mostRecentPromise(queries).then(res => {
+        var queries2 = [buttonBuyService.getNoTimeButton(res[2].value)];
+        mostRecentPromise(queries2).then(res2 => {
+            res.push(res2[0]);
+            response.render('index', { res: res });
         })
-    }, Promise.resolve([]));
-
-    mostRecentPromise.then(res => {
-        response.render('index', { res: res });
     })
+
 });
 
 module.exports = router;
