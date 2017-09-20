b6047916176467484e26a51148bf16fa2bdf65f8
MathieuGi <math.girardon@hotmail.fr>
2017-09-20 16:03:27 +0200

    "/" route - Get all bids and get best bid + render index.ejs with params

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var bidService = require('../services/bids');
 var winston = require('winston');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    winston.log('info', 'Server running')
-    res.render('index', { title: 'Express' });
+router.get('/', function(req, response, next) {
+
+    var queries = [bidService.getBest(), bidService.getAll(), ];
+
+    var mostRecentPromise = queries.reduce((previousPromise, query) => {
+        return previousPromise.then(resultsArray => {
+            return query.then(res => {
+                resultsArray.push(res);
+                return resultsArray;
+            }).catch(err => {
+                resultsArray.push(null);
+                return resultsArray;
+            });
+        })
+    }, Promise.resolve([]));
+
+    mostRecentPromise.then(res => {
+        response.render('index', { res: res });
+    })
+
 });
 
 module.exports = router;
