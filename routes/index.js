f07e40988fea5eb3cad62bd33cabd8c0c75e1b49
MathieuGi <math.girardon@hotmail.fr>
2017-09-21 10:15:34 +0200

    Refacto on route/index.js and views/index.ejs

--- routes/index.js
+++ routes/index.js
@@ -5,34 +5,38 @@ var buttonBuyService = require('../services/buttonBuys');
 var winston = require('winston');
 var FILE_NAME = "routes/index.js";
 
+// Resolve multiple promises and return their results in an array
+var doPromises = function(array) {
+    return array.reduce((previousPromise, promise) => {
+        return previousPromise.then(resultsArray => {
+            return promise.then(res => {
+                winston.info(FILE_NAME + ' - Query success')
+                resultsArray.push(res);
+                return resultsArray;
+            }).catch(err => {
+                winston.warn(FILE_NAME + ' - Nothing retrieve from database')
+                resultsArray.push(null);
+                return resultsArray;
+            });
+        })
+    }, Promise.resolve([]));
+}
+
 /* GET home page. */
 router.get('/', function(req, response, next) {
 
-    var queries = [
+    winston.info(FILE_NAME + ' - Prepare to answer to / request');
+    var promisesArray = [
         bidService.getBest(),
         bidService.getAll(),
         buttonBuyService.getCurrentButton(),
     ];
 
-    var mostRecentPromise = function(array) {
-        return array.reduce((previousPromise, query) => {
-            return previousPromise.then(resultsArray => {
-                return query.then(res => {
-                    resultsArray.push(res);
-                    return resultsArray;
-                }).catch(err => {
-                    winston.info(FILE_NAME + ' - Nothing retrieve from database')
-                    resultsArray.push(null);
-                    return resultsArray;
-                });
-            })
-        }, Promise.resolve([]));
-    }
-
-    mostRecentPromise(queries).then(res => {
-        var queries2 = [buttonBuyService.getNoTimeButton(res[2].value)];
-        mostRecentPromise(queries2).then(res2 => {
+    doPromises(promisesArray).then(res => {
+        var promisesArray2 = [buttonBuyService.getNoTimeButton(res[2].value)];
+        doPromises(promisesArray2).then(res2 => {
             res.push(res2[0]);
+            winston.info(FILE_NAME + ' - Send respond to client');
             response.render('index', { res: res });
         })
     })
