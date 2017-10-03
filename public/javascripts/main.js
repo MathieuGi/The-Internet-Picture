ecd390e1f3d6fd1fa56eb738a0fbe7f8769284f3
MathieuGi <math.girardon@hotmail.fr>
2017-10-03 17:24:55 +0200

    Work on view (effects)

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,4 +6,14 @@ $(document).ready(function(){
 		$('.main-area').hide();
 		$('.' + menu).show();
 	});
+
+	// $('.main-title').fadeTo(1500, 1, function(){
+	// 	$('.img-text .img').fadeTo(1500, 1, function(){
+	// 		$('#menu, .main-article header, .richest-text').fadeTo(2000, 1);
+	// 	});
+	// });
+
+
+	$('.main-article .richest-text').css('bottom', $('.main-article .richest-text').outerHeight());
+	
 });
