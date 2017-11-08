ae2c48b15d72e4dbe110eaff46761ae42e0eb452
DESKTOP-78L3OHJ\PC-Mathieu <math.girardon@hotmail.fr>
2017-11-08 15:40:10 +0100

    Favicon middleware

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ winston.add(winston.transports.File, { filename: 'logger.log' });
 winston.remove(winston.transports.Console);
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/images/static', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -52,18 +52,18 @@ app.use('/popper', express.static(__dirname + '/node_modules/popper.js/dist/'));
 
 // Redirect all request to /
 app.use(function (req, res) {
-  res.redirect("https://localhost:3000/");
+    res.redirect("https://localhost:3000/");
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
