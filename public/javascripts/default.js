c7a98e782944cbf7aed32d75b3efc859fea38599
agayrard <arthur.gayrard@gmail.com>
2017-11-08 14:44:54 +0100

    Button facebook and twitter

--- public/javascripts/default.js
+++ public/javascripts/default.js
@@ -1,5 +1,32 @@
  $(document).ready(function() {
 
+
+    // Usefull for twitter and facebook button
+   (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.10';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
+
     //popover
     $(function () {
       $('[data-toggle="popover"]').popover()
