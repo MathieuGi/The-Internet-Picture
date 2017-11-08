6c7e7b5c78bd614d373486a4cc193a79d2e1ebb6
DESKTOP-78L3OHJ\PC-Mathieu <math.girardon@hotmail.fr>
2017-11-08 13:57:59 +0100

    Indentation

--- routes/index.js
+++ routes/index.js
@@ -11,16 +11,16 @@ var checkType = require('check-types');
 var stripe = require("stripe")("sk_test_XAfsNpri7WZmRNUlmmpopsBS");
 var ejs = require('ejs');
 var sequelize = require('sequelize')
-var returnRouter = function(io) {
+var returnRouter = function (io) {
 
     /* GET home page. */
-    router.get('/', function(req, response, next) {
+    router.get('/', function (req, response, next) {
         winston.info(FILE_NAME + ' - Prepare to answer to / request');
         winston.info(FILE_NAME + ' - Request from ' + req.device.type);
 
         bidService.getAll(10, 0).then(res => {
             winston.info(FILE_NAME + ' - Send respond to client');
-            if(req.device.type === "phone"){
+            if (req.device.type === "phone") {
                 response.render('mobile/index', { bidders: res });
             } else {
                 response.render('index', { bidders: res });
@@ -51,7 +51,7 @@ var returnRouter = function(io) {
     });
 
     // Load more element in rank-table
-    router.get('/getBidsList', function(req, response, next) {
+    router.get('/getBidsList', function (req, response, next) {
         winston.info(FILE_NAME + ' - Prepare to answer to /getBidsList request');
         winston.info(FILE_NAME + ' - Request from ' + req.device.type);
 
@@ -59,25 +59,25 @@ var returnRouter = function(io) {
 
         bidService.getAll(10, offset).then(res => {
             winston.info(FILE_NAME + ' - Send respond to client');
-            if(req.device.type === "phone"){
-                response.render('mobile/rankTableContent', {bidders: res, offset: offset});
+            if (req.device.type === "phone") {
+                response.render('mobile/rankTableContent', { bidders: res, offset: offset });
             } else {
-                response.render('rankTableContent', {bidders: res, offset: offset});
+                response.render('rankTableContent', { bidders: res, offset: offset });
             }
         }).catch(err => {
             winston.error(FILE_NAME + ' - Fail to use bidService.getAll() function: ' + err);
         });
     });
 
-    router.post('/confirmBid', function(req, res, next){
+    router.post('/confirmBid', function (req, res, next) {
 
         // Variables settings
         var body = req.body;
         bidService.getByIdAndToken(body.id, body.token).then(bidder => {
             bidService.getBest().then(bestBidder => {
 
-                if(bidder.price <= bestBidder.price) {
-                    return res.status(500).json({error: "PriceTooLow"})
+                if (bidder.price <= bestBidder.price) {
+                    return res.status(500).json({ error: "PriceTooLow" })
                 } else {
                     var params = body.email !== "" ? {
                         // Send price in centimes
@@ -87,22 +87,22 @@ var returnRouter = function(io) {
                         description: "Nouvelle enchère",
                         source: bidder.transaction_id,
                     } : {
-                        // Send price in centimes
-                        amount: bidder.price * 100,
-                        currency: "eur",
-                        description: "Nouvelle enchère",
-                        source: bidder.transaction_id,
-                    }
-                    stripe.charges.create(params, function(err, charge) {
+                            // Send price in centimes
+                            amount: bidder.price * 100,
+                            currency: "eur",
+                            description: "Nouvelle enchère",
+                            source: bidder.transaction_id,
+                        }
+                    stripe.charges.create(params, function (err, charge) {
                         if (err) {
                             winston.error(FILE_NAME + ' - Paiement failed:' + err);
                             return res.status(500).json({ error: 'paiementFailed' });
                         } else {
                             bidService.setBidTime().then(() => {
-                                bidService.setActive(bidder.id).then(function(){
+                                bidService.setActive(bidder.id).then(function () {
                                     sharedService.emitNewBidder(io);
-                                    return res.status(200).json({ result: 'success' });      
-                                });                      
+                                    return res.status(200).json({ result: 'success' });
+                                });
                             });
                         }
                     });
@@ -112,7 +112,7 @@ var returnRouter = function(io) {
             });
         }).catch(err => {
             winston.info(FILE_NAME + ' - Fail to use bidService.getById() function: ' + err);
-            return res.status(500).json({ error: 'IdNotFound'})
+            return res.status(500).json({ error: 'IdNotFound' })
         });
     });
 
@@ -120,7 +120,7 @@ var returnRouter = function(io) {
     var upload = multer({ dest: 'public/images/fullsize' });
 
     // Post request to create a new bid
-    router.post('/createBid', upload.single('image'), function(req, res, next) {
+    router.post('/createBid', upload.single('image'), function (req, res, next) {
         winston.info(FILE_NAME + ' - Prepare to answer to /createBid request');
 
         // Variables settings
@@ -132,7 +132,7 @@ var returnRouter = function(io) {
             return res.status(500).json({ error: "missingField" });
         }
 
-        if(typeof req.file == 'undefined'){
+        if (typeof req.file == 'undefined') {
             winston.error(FILE_NAME + ' - Trying to create bid without image. Request canceled');
             return res.status(500).json({ error: "noImage" });
         }
@@ -148,34 +148,34 @@ var returnRouter = function(io) {
             return res.status(500).json({ error: "wrongType" });
         } else {
 
-           // Check variables types
+            // Check variables types
             if (!(checkType.string(body.name) &&
-                    checkType.string(body.url) &&
-                    checkType.string(body.text) &&
-                    checkType.string(body.token) &&
-                    checkType.integer(parseInt(body.price, 10)))
-                ) {
+                checkType.string(body.url) &&
+                checkType.string(body.text) &&
+                checkType.string(body.token) &&
+                checkType.integer(parseInt(body.price, 10)))
+            ) {
 
                 winston.error(FILE_NAME + ' - Trying to create bid with wrong type of variables');
                 return res.status(500).json({ error: "wrongFieldsType" });
             }
 
-            if(body.name.length > 40 || body.text.length > 130){
+            if (body.name.length > 40 || body.text.length > 130) {
                 winston.error(FILE_NAME + ' - Trying to create bid with too long fields');
                 return res.status(500).json({ error: "wrongFieldsType" });
             }
 
             // Save image in folder (fullsize and resized)
-            sharedService.saveImage(req.file, req.file.filename).then(function(){
+            sharedService.saveImage(req.file, req.file.filename).then(function () {
                 // Create the new bid
-                bidService.create(body.name, newName, body.url, body.text, parseInt(body.price), body.token).then(function(newBid) {
+                bidService.create(body.name, newName, body.url, body.text, parseInt(body.price), body.token).then(function (newBid) {
                     winston.info('this is the smiley : ' + newBid.name);
-                    if(req.device.type === "phone"){
-                        return res.status(200).render('mobile/confirmBid', {bidder: newBid});
+                    if (req.device.type === "phone") {
+                        return res.status(200).render('mobile/confirmBid', { bidder: newBid });
                     } else {
-                        return res.status(200).render('confirmBid', {bidder: newBid});
+                        return res.status(200).render('confirmBid', { bidder: newBid });
                     }
-                }).catch(function(err) {
+                }).catch(function (err) {
                     winston.error(FILE_NAME + ' - Fail to add new bid in database');
                     winston.error(FILE_NAME + ' - create bid: ' + err)
                     return res.status(500).json({ error: 'creationFailed' });
