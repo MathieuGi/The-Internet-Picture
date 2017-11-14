fad7cf4d70b78671fe87eb7c2ef6f817b5e9f665
DESKTOP-78L3OHJ\PC-Mathieu <math.girardon@hotmail.fr>
2017-11-14 16:15:54 +0100

    fb share

--- app.js
+++ app.js
@@ -42,8 +42,6 @@ app.use(device.capture());
 
 device.enableViewRouting(app);
 
-app.use(require('prerender-node').set('prerenderToken', 'mvhWhKnEbXUpP5VxKNHv'));
-
 app.use('/', index);
 app.use('/users', users);
 
