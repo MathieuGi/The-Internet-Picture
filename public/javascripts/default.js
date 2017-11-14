f67c00c8794d34b093e8eec3ce40153efee90488
DESKTOP-78L3OHJ\PC-Mathieu <math.girardon@hotmail.fr>
2017-11-14 16:39:22 +0100

    http test share button fb

--- public/javascripts/default.js
+++ public/javascripts/default.js
@@ -144,13 +144,7 @@ $(document).ready(function () {
   });
 
   // Footer -> twitter and facebook buttons
-  (function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/fr_FR/sdk/debug.js#xfbml=1&version=v2.11';
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+
 
   window.twttr = (function (d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0],
